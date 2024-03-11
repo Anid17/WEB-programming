584ad578433eedfc687f958703c70d14b58c7538
Anid Ali <anid.ali@stu.ibu.edu.ba>
2024-03-11 12:54:37 +0100

    Anids message

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'yourusername/web-project',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'c70a15852c7c2aca1a1d94ce9fe8ecc5dee25ce5',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mikecao/flight' => array(
+            'pretty_version' => 'v3.6.0',
+            'version' => '3.6.0.0',
+            'reference' => '735dafefb9b49ef46f26a511efe224d575c28cc7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../mikecao/flight',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'yourusername/web-project' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'c70a15852c7c2aca1a1d94ce9fe8ecc5dee25ce5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
